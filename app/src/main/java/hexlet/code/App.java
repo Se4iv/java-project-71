3df0a25cb2d470ba2877cc608e258effdfb7f9f0
Sergey Ivanov <beckron@mail.ru>
2023-10-10 20:58:21 +0300

    step picoli cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,12 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
+    public static void main(String[] args) throws Exception {
+       int exitcode = new CommandLine(new App()).execute(args);
+       System.exit(exitcode);
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
